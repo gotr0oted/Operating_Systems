1ff30dbe60191e66baf4f0c5eb10f052560dddc0
Parth <parthpusadkar1312@gmail.com>
2026-04-15 21:08:07 +0530

    Update code.sh

--- Practical 13/code.sh
+++ Practical 13/code.sh
@@ -1,1 +1,26 @@
+echo "Enter two numbers:"
+read a b
+sum=$((a + b))
+echo "Sum = $sum"
+
+echo "Enter two numbers:"
+read a b
+sum=$((a + b))
+echo "Sum = $sum"
+
+echo "Enter your country:"
+read country
+
+if [ "$country" == "India" ]
+then
+    echo "Namaste!"
+elif [ "$country" == "USA" ]
+then
+    echo "Hello!"
+elif [ "$country" == "Japan" ]
+then
+    echo "Konnichiwa!"
+else
+    echo "Hello from $country!"
+fi
 
