2755e6d96a35d78467a273cd37e5538b2e397351
Parth <parthpusadkar1312@gmail.com>
2026-04-15 21:13:25 +0530

    Update extra.sh

--- Practical 15/extra.sh
+++ Practical 15/extra.sh
@@ -1,1 +1,50 @@
+echo "Choose an option:"
+echo "a) Leap Year"
+echo "b) Even or Odd"
+echo "c) Largest of 3 Numbers"
+read choice
 
+case $choice in
+    a)
+        echo "Enter year:"
+        read year
+
+        if (( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ))
+        then
+            echo "$year is a Leap Year"
+        else
+            echo "$year is NOT a Leap Year"
+        fi
+        ;;
+    
+    b)
+        echo "Enter a number:"
+        read num
+
+        if (( num % 2 == 0 ))
+        then
+            echo "$num is Even"
+        else
+            echo "$num is Odd"
+        fi
+        ;;
+
+    c)
+        echo "Enter three numbers:"
+        read a b c
+
+        if (( a >= b && a >= c ))
+        then
+            echo "$a is largest"
+        elif (( b >= a && b >= c ))
+        then
+            echo "$b is largest"
+        else
+            echo "$c is largest"
+        fi
+        ;;
+
+    *)
+        echo "Invalid choice"
+        ;;
+esac
